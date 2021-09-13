0cd7562769dbd847369e33862aa4c26daddcce19
Foram Patel <86473714+foram147@users.noreply.github.com>
2021-09-13 19:45:26 +0530

    Add files via upload

---
+++ m3-d1-extra.js
@@ -0,0 +1,198 @@
+/* 1) MAX CHAR
+
+Given a string, return the character that is most
+commonly used in the string.
+
+--- Examples
+    maxChar("abcccccccd") === "c"
+    maxChar("apple 1231111") === "1"
+*/
+function getMost(str) {
+  let most = 0;
+  let maxLength = "";
+  str.split("").forEach(function (str1) {
+    if (str.split(str1).length > most) {
+      most = str.split(str1).length;
+      maxLength = str1;
+    }
+  });
+  return maxLength;
+}
+console.log("-----------------EX1----------------");
+console.log(getMost("helooooooo"));
+/* 2) ANAGRAMS
+
+Check to see if two provided strings are anagrams of each other.
+One string is an anagram of another if it uses the same characters
+in the same quantity. Only consider characters, not spaces
+or punctuation.  Consider capital letters to be the same as lower case
+
+--- Examples
+  anagrams('rail safety', 'fairy tales') --> True
+  anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+  anagrams('Hi there', 'Bye there') --> False
+*/
+
+/* 3) ANAGRAMS 2
+
+Given a word and a list of possible anagrams, select the correct sublist.
+
+--- Examples 
+
+    "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
+*/
+
+/* 4) PALINDROME
+
+Given a string, return true if the string is a palindrome
+or false if it is not.  Palindromes are strings that
+form the same word if it is reversed. Do include spaces
+and punctuation in determining if the string is a palindrome.
+
+--- Examples:
+
+    palindrome("abba") === true
+    palindrome("abcdefg") === false
+ */
+function checkPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+console.log("-----------------EX4----------------");
+console.log(checkPalindrome("abba"));
+/* 5) REVERSE INT
+
+Given an integer, return an integer that is the reverse
+ordering of numbers.
+
+--- Examples
+
+    reverseInt(15) === 51
+    reverseInt(981) === 189
+    reverseInt(500) === 5
+    reverseInt(-15) === -51
+    reverseInt(-90) === -9
+ */
+function getReversed(n) {
+  let value = n.toString().split("").reverse().join("");
+  return Math.sign(n) * parseInt(value);
+}
+console.log("-----------------EX5----------------");
+console.log(getReversed(578));
+
+/* 6) STEPS
+
+Write a function that accepts a positive number N.
+The function should console log a step shape
+with N levels using the # character.  Make sure the
+step has spaces on the right hand side!
+
+--- Examples
+
+    steps(2)
+        '# '
+        '##'
+    steps(3)
+        '#  '
+        '## '
+        '###'
+    steps(4)
+        '#   '
+        '##  '
+        '### '
+        '####' */
+
+function getSteps(n) {
+  let display = "#";
+  for (let i = 0; i <= n - 1; i++) {
+    for (let j = 1; j <= i; j++) {
+      display += "#";
+    }
+    console.log(display);
+  }
+}
+console.log("-----------------EX6----------------");
+getSteps(2);
+
+/* 7) REVERSE STRING
+
+Given a string, return a new string with the reversed
+order of characters
+
+--- Examples
+
+    reverse('apple') === 'leppa'
+    reverse('hello') === 'olleh'
+    reverse('Greetings!') === '!sgniteerG'
+ */
+function getReversedStr(str) {
+  let value = str.split("").reverse().join("");
+  return value;
+}
+console.log("-----------------EX7----------------");
+console.log(getReversedStr("apple"));
+/* 8) CHUNK
+
+Given an array and chunk size, divide the array into many subarrays
+where each subarray is of length size
+
+--- Examples
+
+    chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+    chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+    chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+    chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+    chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+*/
+
+/* 9) PYRAMID
+
+Write a function that accepts a positive number N.
+The function should console log a pyramid shape
+with N levels using the # character.  Make sure the
+pyramid has spaces on both the left and right hand sides
+
+--- Examples
+
+    pyramid(1)
+        '#'
+    pyramid(2)
+        ' # '
+        '###'
+    pyramid(3)
+        '  #  '
+        ' ### '
+        '#####' */
+function pyramid(n) {
+  for (let i = 1; i <= n; i++) {
+    let str = " ".repeat(n - i);
+
+    let str2 = "#".repeat(i * 2 - 1);
+
+    console.log(str + str2 + str);
+  }
+}
+console.log("-----------------EX9----------------");
+pyramid(3);
+
+/* 10) SPYRAL MATRIX
+
+Write a function that accepts an integer N
+and returns a NxN spiral matrix.
+
+--- Examples
+
+    matrix(2)
+        [[1, 2],
+        [4, 3]]
+    matrix(3)
+        [[1, 2, 3],
+        [8, 9, 4],
+        [7, 6, 5]]
+    matrix(4)
+        [[1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10,  9,  8, 7]]
+
+*/
--- m3-d1.js
+++ m3-d1.js
@@ -6,6 +6,7 @@ Create a function to calculate the sum of the two given integers. If the two val
 function calculate(a, b) {
   return a === b ? (a + b) * 3 : a + b;
 }
+console.log("-----------------EX1----------------");
 console.log(calculate(3, 3));
 /*
 2)
@@ -14,6 +15,7 @@ Create a function to check two given numbers and return true if one of the numbe
 function checkNum(c, d) {
   return c === 50 || d === 50 || c + d == 50 ? true : false;
 }
+console.log("-----------------EX2----------------");
 console.log(checkNum(30, 50));
 /*
 3)
@@ -24,6 +26,7 @@ function removeChar(str, n) {
   let newStr = str.substr(n + 1, str.length);
   return oldStr + newStr;
 }
+console.log("-----------------EX3----------------");
 console.log(removeChar("foram", 2));
 /*
 
@@ -42,11 +45,11 @@ function largestVal(e, f, g) {
   }
   return num;
 }
-
+console.log("-----------------EX4----------------");
 console.log(largestVal(50, 9, 1));
 /*
 5)
-Create a function to check whether two numbers are in range 40..60 or in the range 70..100 inclusive.
+Create a function to check if two numbers are in the range 40-60 or 70-100. Return true if they do, return false if one (or both) don't.
 */
 function checkRange(h, i) {
   return (h >= 40 && h <= 60 && i >= 40 && i <= 60) ||
@@ -54,21 +57,23 @@ function checkRange(h, i) {
     ? true
     : false;
 }
+console.log("-----------------EX5----------------");
 console.log(checkRange(75, 100));
 /*
 6) 
 
-Create a function to create a new string of specified copies (positive number) of a given string.
+Create a function to create a new string composed of a specified number of copies of a given string. Pass the string and the number of copies as parameters.
 */
 function copyStr(n) {
   let str = "foram";
   return n > 0 ? console.log(str.repeat(n)) : console.log("enter valid num");
 }
+console.log("-----------------EX6----------------");
 copyStr(5);
 /*
 
 7)
-Create a function to display the city name if the string begins with "Los" or "New" otherwise return blank.
+Create a function to display the city name if the string begins with "Los" or "New". Pass the city name as a parameter. Return false if they start with a different string.
 */
 function cityName(str2) {
   return str2.length >= 3 &&
@@ -76,11 +81,12 @@ function cityName(str2) {
     ? str2
     : false;
 }
+console.log("-----------------EX7----------------");
 console.log(cityName("Bombay"));
 
 /*
 8)
-Create a function to calculate the sum of three elements of a given array of integers of length 3.
+Create a function to calculate and return the sum of all elements from an array with 3 elements. Pass the array as a parameter.
 */
 function totalArray() {
   let arr = [1, 2, 3];
@@ -89,11 +95,12 @@ function totalArray() {
   }, 0);
   return total;
 }
+console.log("-----------------EX8----------------");
 console.log(totalArray());
 /*
 
 9)
-Create a function to test whether an array of integers of length 2 contains 1 or a 3. 
+Create a function to test whether an array of integers of length 2 contains 1 or a 3.Return true is it does, false if it doesn't. 
 */
 function checkArr() {
   let arr = [5, 6, 7];
@@ -105,12 +112,12 @@ function checkArr() {
     }
   }
 }
+console.log("-----------------EX9----------------");
 console.log(checkArr());
 /*
 
 10)
-
-Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
+Create a function to test if an array of lenght 2 DOES NOT contain 1 or 3. Return true if it doesn't, false if it does
 */
 function checkArr1(arr1) {
   if (arr1.indexOf(1) == -1 && arr1.indexOf(3) == -1) {
@@ -119,12 +126,12 @@ function checkArr1(arr1) {
     return false;
   }
 }
-
+console.log("-----------------EX10----------------");
 console.log(checkArr1([5, 4]));
 
 /*11)
 
-Create a function to find the longest string from a given array of strings.
+Create a function to find the longest string from a given array of strings. Pass the array as parameter and return the longest string.
 */
 function findLongStr(strArr) {
   let arr2 = 0;
@@ -136,17 +143,18 @@ function findLongStr(strArr) {
   }
   return arr2;
 }
-console.log(findLongStr(["a", "ab", "abc"]));
+console.log("-----------------EX11----------------");
+console.log(findLongStr(["f", "fo", "for"]));
 /*
 12)
 
 Create a function to find the types of a given angle.
 
 Types of angles:
-    Acute angle: An angle between 0 and 90 degrees.
-    Right angle: An 90 degree angle.
-    btuse angle: An angle between 90 and 180 degrees.
-    Straight angle: A 180 degree angle.*/
+    Acute angle: An angle between 0 and 90 degrees.Return Acute
+    Right angle: An 90 degree angle. Return Right
+    Obtuse angle: An angle between 90 and 180 degrees. Return Obtuse
+    Straight angle: A 180 degree angle.Return Straight*/
 function angle(a) {
   if (a > 0 && a < 90) {
     console.log(" Acute angle: An angle between 0 and 90 degrees.");
@@ -163,10 +171,11 @@ function angle(a) {
 
   return a;
 }
+console.log("-----------------EX12----------------");
 angle(119);
-/*13)
 
-Create a function to find the index of the greatest element of a given array of integers*/
+/*13)
+Create a function to find and return the index of the greatest element of a given array of integers that you passed as a parameter*/
 function getIndex(arrIndex) {
   let element = "";
   let index = "";
@@ -178,10 +187,10 @@ function getIndex(arrIndex) {
   }
   return index;
 }
+console.log("-----------------EX13----------------");
 console.log(getIndex([1, 12, 3]));
 /*14)
-
-Create a function to get the largest even number from an array of integers.
+Create a function to find and return the largest even number from an array of integers that is passed a parameter.
 */
 function largestEven(arrEven) {
   let element = "";
@@ -193,60 +202,53 @@ function largestEven(arrEven) {
   }
   return element;
 }
+console.log("-----------------EX14----------------");
 console.log(largestEven([10, 2, 18]));
 /*
-16)
-
-Create a function to check from two given integers, whether one is positive and another one is negative.*/
+15)
+Create a function to check from two given integers (passed as parameters) if one is positive and the other is negative. Return true if that's the case, return false if it's not.*/
 function checkPositive(a, b) {
   return (a > 0 && b < 0) || (a < 0 && b > 0) ? true : false;
 }
+console.log("-----------------EX15----------------");
 console.log(checkPositive(10, -5));
 
 /*
+16)
 
-17)
-
-Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
+Create a function to create and return a new string where the first 3 characters are in lower case and the others are in upper case. If the string's length is less than 3, convert the whole string into uppercase. Pass the original string as a parameter.
 */
 function checkString(str3) {
-  if (str3 < 3) {
+  if (str3.length < 3) {
     return str3.toUpperCase();
   }
   let beginning = str3.substring(0, 3).toLowerCase();
-  let ending = str3.substring(3, str3.length);
+  let ending = str3.substring(3).toUpperCase();
   return beginning + ending;
 }
+console.log("-----------------EX16----------------");
 console.log(checkString("foram"));
-/*18)
 
+/*17)
 Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
-
 */
 function findSum(a, b) {
   const sum = a + b;
-  if (sum >= 50 && sum <= 80) {
-    return 65;
-  }
-  return 80;
+  return sum >= 50 && sum <= 80 ? 65 : 80;
 }
-console.log(findSum(30, 20));
+console.log("-----------------EX17----------------");
+console.log(findSum(30, 10));
 
-/*19)
+/*18)
 
-Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
-
-If the number has 3 as a factor, output 'Diego'.
-If the number has 5 as a factor, output 'Riccardo'.
-If the number has 7 as a factor, output 'Stefano'.
-If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-Examples
-28's factors are 1, 2, 4, 7, 14, 28.
-this would be a simple "Stefano".
-30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
-this would be a "DiegoRiccardo".
-34 has four factors: 1, 2, 17, and 34.
-this would be "34".
+Create a function to convert a number (passed as a parameter) into a string, basing yourself on this example: 
+The number has 3 as a factor ⇒ return Diego 
+The number has 5 as a factor ⇒ return Riccardo 
+The number has 7 as a factor ⇒ return Stefano 
+If the number does not have 3,5, or 7, return the original number. 
+⚠️ The factor is an integer which evenly divides a number without leaving a remainder. 
+One number can have more than one factor, in that case you should return both names. 
+Ex: 15 has both 3 and 5 has factors: the function will return DiegoRiccardo
 
 */
 function numberFactors(getNum) {
@@ -264,8 +266,9 @@ function numberFactors(getNum) {
 
   return number;
 }
-console.log(numberFactors(5));
-/*20)
+console.log("-----------------EX18----------------");
+console.log(numberFactors(15));
+/*19)
 Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
 
 */
@@ -277,4 +280,5 @@ function getAcronym(str) {
   var acr = words.map(getDigit);
   return acr.join("").toUpperCase();
 }
+console.log("-----------------EX19----------------");
 console.log(getAcronym("hello world"));
